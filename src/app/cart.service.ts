c0d645b850e0efff19a12472c4fd3a9d05ac9442
cnmediar <mediar@qq.com>
2022-12-16 17:50:00 +0800

    5 files modified, 8 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,31 @@
+import { Product } from './products';
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+/* . . . */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  items: Product[] = [];
+  /* . . . */
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
 }
